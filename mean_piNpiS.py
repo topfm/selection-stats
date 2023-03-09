5e82480fd323be66e421a5e7d853ce57c8f3e0b2
Madeline Topf <madelinetopf@Madelines-MacBook-Pro-2.local>
2023-03-09 15:49:10 -0600

    adding averaging script + changing names

---
+++ mean_piNpiS.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+import sys
+import numpy
+
+##check for correct args
+if len(sys.argv) != 2:
+    print("Usage: mean_piNpiS.py selectionStats_piNpiS.txt")
+    sys.exit(0)
+
+statsfile = sys.argv[1]
+
+
+##read genes into file with dict = {gene1:[val1,val2,val3], gene2:[val1,val2,val3]}
+##Here, I am skipping any piNpiS value of "None", which indicates that the codon contains an "N" (instead of ACTG)
+dd = defaultdict(list)
+avDict = {}
+with open(statsfile, "r") as infile:
+    next(infile)
+    for line in infile:
+        if "None" in line:
+            pass
+        else:
+            info = line.strip().split("\t")
+            gene= info[0]
+            gene = gene.split("_")
+            piNpiS = info[-1]
+            if len(gene) > 3:
+                gene = gene[0] + "_" + gene[1]
+            else:
+                gene = gene[0]
+            dd[gene].append(piNpiS)
+        
+
+##Averaging each set of values for each key in the dictionary
+##This will give an average piNpiS value for each gene!
+for k, v in dd.items():
+    if v != "NA":
+        v = [(float(i)) for i in v]
+        mean = sum(v) / len(v)
+        avDict[k] = round(mean, 2)
+
+##Writing the output file
+outfile = statsfile.replace(".txt", "_average.csv") 
+
+with open(outfile, "w") as f:
+     for key in avDict.keys():
+        f.write("%s, %s\n" %(key, avDict[key]))
+   
+
+
+
+
